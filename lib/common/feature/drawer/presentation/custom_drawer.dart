187d1006b2539cb551555165cb27b6fb1a59970d
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-12 14:34:59 +0330

    Switch for l10n changed to SegmentedController.

--- lib/common/feature/drawer/presentation/custom_drawer.dart
+++ lib/common/feature/drawer/presentation/custom_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_test/common/feature/drawer/presentation/bloc/drawer_bloc.dart';
@@ -14,8 +15,7 @@ class CustomDrawer extends StatefulWidget {
 }
 
 class _CustomDrawerState extends State<CustomDrawer> {
-  bool isEN = true;
-  Set<int> selection = <int>{0, 1};
+  String selectedValue = LanguageManager.shared.ENGLISH;
 
   @override
   void initState() {
@@ -23,11 +23,11 @@ class _CustomDrawerState extends State<CustomDrawer> {
     LanguageManager.shared.getLocale().then((locale) {
       if (locale.languageCode == LanguageManager.shared.ENGLISH) {
         setState(() {
-          isEN = true;
+          selectedValue = LanguageManager.shared.ENGLISH;
         });
       } else {
         setState(() {
-          isEN = false;
+          selectedValue = LanguageManager.shared.FARSI;
         });
       }
     });
@@ -37,6 +37,18 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
+    Map<String, Container> segmentChildren = {
+      LanguageManager.shared.ENGLISH: buildSegmentContainer(
+        context,
+        themeData,
+        LanguageManager.shared.translation(context).english,
+      ),
+      LanguageManager.shared.FARSI: buildSegmentContainer(
+        context,
+        themeData,
+        LanguageManager.shared.translation(context).farsi,
+      ),
+    };
     return Drawer(
       child: ListView(
         padding: const EdgeInsets.all(
@@ -140,21 +152,16 @@ class _CustomDrawerState extends State<CustomDrawer> {
                 ),
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    LanguageManager.shared.translation(context).farsi,
-                    style: themeData.textTheme.titleMedium!.copyWith(
-                      color: const Color.fromARGB(229, 255, 255, 255),
-                    ),
-                  ),
-                  Switch(
-                    value: isEN,
-                    onChanged: (val) async {
+                  CupertinoSegmentedControl(
+                    groupValue: selectedValue,
+                    children: segmentChildren,
+                    onValueChanged: (value) async {
                       setState(() {
-                        isEN = val;
+                        selectedValue = value;
                       });
-                      if (val) {
+                      if (value == LanguageManager.shared.ENGLISH) {
                         Locale locale = await LanguageManager.shared
                             .setLocale(LanguageManager.shared.ENGLISH);
                         // ignore: use_build_context_synchronously
@@ -166,11 +173,11 @@ class _CustomDrawerState extends State<CustomDrawer> {
                         MyApp.setLocale(context, locale);
                       }
                     },
-                  ),
-                  Text(
-                    LanguageManager.shared.translation(context).english,
-                    style: themeData.textTheme.titleMedium!.copyWith(
-                      color: const Color.fromARGB(229, 255, 255, 255),
+                    selectedColor: const Color.fromARGB(255, 147, 10, 65),
+                    unselectedColor: const Color.fromARGB(255, 36, 37, 56),
+                    borderColor: CupertinoColors.inactiveGray,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
                     ),
                   ),
                 ],
@@ -199,6 +206,25 @@ class _CustomDrawerState extends State<CustomDrawer> {
             },
           ),
         ],
+      ),
+    );
+  }
+
+  Container buildSegmentContainer(
+    BuildContext context,
+    ThemeData themeData,
+    String language,
+  ) {
+    Size size = MediaQuery.sizeOf(context);
+    return Container(
+      width: size.width * 0.33,
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Text(
+        language,
+        textAlign: TextAlign.center,
+        style: themeData.textTheme.titleMedium!.copyWith(
+          color: const Color.fromARGB(229, 255, 255, 255),
+        ),
       ),
     );
   }
