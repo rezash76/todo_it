dbe097697fe9846f29267e8ba81f6eb142e24262
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-13 16:39:10 +0330

    operator added to base entity class.

--- lib/common/entity/entity.dart
+++ lib/common/entity/entity.dart
@@ -1,5 +1,20 @@
-abstract class Entity {
+abstract base class Entity {
   final String id;
 
   Entity(this.id);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! Entity) {
+      return false;
+    }
+
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other.id == id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
 }
