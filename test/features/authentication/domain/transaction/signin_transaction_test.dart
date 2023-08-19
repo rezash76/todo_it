3a0fd32cc393fa1adea1aaf9a1020641716fa406
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-19 13:31:42 +0330

    signin transaction tests completed.

--- test/features/authentication/domain/transaction/signin_transaction_test.dart
+++ test/features/authentication/domain/transaction/signin_transaction_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:todo_test/common/error/cache.dart';
 import 'package:todo_test/common/value_object/void_operation.dart';
 import 'package:todo_test/features/athentication/domain/transaction/signin_transaction.dart';
 import 'package:todo_test/features/athentication/domain/value_object/signin_request.dart';
@@ -10,11 +11,19 @@ import 'check_registration_transaction_test.mocks.dart';
 void main() {
   late SigninTransaction signinTransaction;
   late MockUserRepository mockUserRepository;
-  var request = SigninRequest('username', 'password');
+  late SigninRequest request;
+  // exception
+  late WrongPassword wrongPassword;
+  late TypeMissmatch typeMissmatch;
+  late HiveException hiveException;
 
   setUp(() {
     mockUserRepository = MockUserRepository();
     signinTransaction = SigninTransaction(repository: mockUserRepository);
+    request = SigninRequest('username', 'password');
+    wrongPassword = WrongPassword();
+    typeMissmatch = TypeMissmatch();
+    hiveException = HiveException();
   });
 
   group('Signin transaction', () {
@@ -25,6 +34,36 @@ void main() {
       var result = await signinTransaction.call(request);
       verify(mockUserRepository.signin(request));
       expect(result, Right(VoidOperation().voidValue));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return WrongPassword Exception when pass is wrong.', () async {
+      when(mockUserRepository.signin(any))
+          .thenAnswer((_) async => Left(wrongPassword));
+
+      var result = await signinTransaction.call(request);
+      verify(mockUserRepository.signin(request));
+      expect(result, Left(wrongPassword));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return TypeMissmatch Exception.', () async {
+      when(mockUserRepository.signin(any))
+          .thenAnswer((_) async => Left(typeMissmatch));
+
+      var result = await signinTransaction.call(request);
+      verify(mockUserRepository.signin(request));
+      expect(result, Left(typeMissmatch));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return HiveException.', () async {
+      when(mockUserRepository.signin(any))
+          .thenAnswer((_) async => Left(hiveException));
+
+      var result = await signinTransaction.call(request);
+      verify(mockUserRepository.signin(request));
+      expect(result, Left(hiveException));
       verifyNoMoreInteractions(mockUserRepository);
     });
   });
