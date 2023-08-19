d0c1f1ba6b9d7a8beee66ca39f4b652600415c21
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-19 13:40:03 +0330

    signup transaction tests completed.

--- test/features/authentication/domain/transaction/signup_transaction_test.dart
+++ test/features/authentication/domain/transaction/signup_transaction_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:todo_test/common/error/cache.dart';
 import 'package:todo_test/common/value_object/void_operation.dart';
 import 'package:todo_test/features/athentication/domain/transaction/signup_transaction.dart';
 import 'package:todo_test/features/athentication/domain/value_object/signup_request.dart';
@@ -16,10 +17,15 @@ void main() {
     username: 'username',
     password: 'password',
   );
+  // exceptin
+  late TypeMissmatch typeMissmatch;
+  late HiveException hiveException;
 
   setUp(() {
     mockUserRepository = MockUserRepository();
     signupTransaction = SignupTransaction(repository: mockUserRepository);
+    typeMissmatch = TypeMissmatch();
+    hiveException = HiveException();
   });
 
   group('Signup transaction', () {
@@ -30,6 +36,26 @@ void main() {
       var result = await signupTransaction.call(request);
       verify(mockUserRepository.signup(request));
       expect(result, Right(VoidOperation().voidValue));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return TypeMissmatch Exception.', () async {
+      when(mockUserRepository.signup(any))
+          .thenAnswer((_) async => Left(typeMissmatch));
+
+      var result = await signupTransaction.call(request);
+      verify(mockUserRepository.signup(request));
+      expect(result, Left(typeMissmatch));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return HiveException.', () async {
+      when(mockUserRepository.signup(any))
+          .thenAnswer((_) async => Left(hiveException));
+
+      var result = await signupTransaction.call(request);
+      verify(mockUserRepository.signup(request));
+      expect(result, Left(hiveException));
       verifyNoMoreInteractions(mockUserRepository);
     });
   });
