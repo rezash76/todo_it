3bdc8da0cbbf70cb05631378deb473189bc10fc1
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-19 13:59:45 +0330

    add new task transaction tests completed.

--- test/features/todo/domain/transaction/add_new_task_transaction_test.dart
+++ test/features/todo/domain/transaction/add_new_task_transaction_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:todo_test/common/error/cache.dart';
 import 'package:todo_test/features/todo/domain/entity/task_entity.dart';
 import 'package:todo_test/features/todo/domain/transaction/add_new_task_transaction.dart';
 import 'package:todo_test/features/todo/domain/value_object/task_request.dart';
@@ -10,18 +11,22 @@ import 'add_new_task_transaction_test.mocks.dart';
 void main() {
   late AddNewTaskTransaction addNewTaskTransaction;
   late MockTaskRepository mockTaskRepository;
-  List<TaskEntity> tasks = [
-    TaskEntity('asd', 'asef', false),
-  ];
+  late List<TaskEntity> tasks;
+  late TaskRequest request;
+  // exception
+  late TypeMissmatch typeMissmatch;
+  late HiveException hiveException;
 
-  var request = TaskRequest('Hi', 'desc', false);
+  setUp(() {
+    mockTaskRepository = MockTaskRepository();
+    addNewTaskTransaction = AddNewTaskTransaction(mockTaskRepository);
+    tasks = [TaskEntity('asd', 'asef', false)];
+    request = TaskRequest('Hi', 'desc', false);
+    typeMissmatch = TypeMissmatch();
+    hiveException = HiveException();
+  });
 
   group('add new task transaction', () {
-    setUp(() {
-      mockTaskRepository = MockTaskRepository();
-      addNewTaskTransaction = AddNewTaskTransaction(mockTaskRepository);
-    });
-
     test('should return List<TaskEntity>', () async {
       when(mockTaskRepository.addNewTask(any))
           .thenAnswer((_) async => Right(tasks));
@@ -29,6 +34,26 @@ void main() {
       var result = await addNewTaskTransaction.call(request);
       verify(mockTaskRepository.addNewTask(request));
       expect(result, Right(tasks));
+      verifyNoMoreInteractions(mockTaskRepository);
+    });
+
+    test('should return TypeMissmatch Exception.', () async {
+      when(mockTaskRepository.addNewTask(any))
+          .thenAnswer((_) async => Left(typeMissmatch));
+
+      var result = await addNewTaskTransaction.call(request);
+      verify(mockTaskRepository.addNewTask(request));
+      expect(result, Left(typeMissmatch));
+      verifyNoMoreInteractions(mockTaskRepository);
+    });
+
+    test('should return HiveException.', () async {
+      when(mockTaskRepository.addNewTask(any))
+          .thenAnswer((_) async => Left(hiveException));
+
+      var result = await addNewTaskTransaction.call(request);
+      verify(mockTaskRepository.addNewTask(request));
+      expect(result, Left(hiveException));
       verifyNoMoreInteractions(mockTaskRepository);
     });
   });
