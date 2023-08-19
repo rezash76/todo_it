5d81ac278852b59e058651185734e97990a58a45
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-19 13:21:41 +0330

    check registration tests completed.

--- test/features/authentication/domain/transaction/check_registration_transaction_test.dart
+++ test/features/authentication/domain/transaction/check_registration_transaction_test.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mockito/mockito.dart';
+import 'package:todo_test/common/error/cache.dart';
 import 'package:todo_test/common/value_object/no_request.dart';
 import 'package:todo_test/features/athentication/domain/entity/user_entity.dart';
 import 'package:todo_test/features/athentication/domain/transaction/check_registration_transaction.dart';
@@ -11,18 +12,23 @@ import 'check_registration_transaction_test.mocks.dart';
 void main() {
   late CheckRegistrationTransaction checkRegistrationTransaction;
   late MockUserRepository mockUserRepository;
-  UserEntity user = UserEntity(
-    'name',
-    'family',
-    'username',
-    'password',
-    true,
-  );
+  late UserEntity user;
+  late NotFound notFoundException;
+  late HiveException hiveException;
 
   setUp(() {
     mockUserRepository = MockUserRepository();
     checkRegistrationTransaction =
         CheckRegistrationTransaction(mockUserRepository);
+    notFoundException = NotFound();
+    hiveException = HiveException();
+    user = UserEntity(
+      'name',
+      'family',
+      'username',
+      'password',
+      true,
+    );
   });
 
   group('Check registraion transaction', () {
@@ -33,6 +39,26 @@ void main() {
       var result = await checkRegistrationTransaction.call(NoRequest());
       verify(mockUserRepository.isUserExist());
       expect(result, Right(user));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return NotFoundException when user not exist.', () async {
+      when(mockUserRepository.isUserExist())
+          .thenAnswer((_) async => Left(notFoundException));
+
+      var result = await checkRegistrationTransaction.call(NoRequest());
+      verify(mockUserRepository.isUserExist());
+      expect(result, Left(notFoundException));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return HiveException when throws HiveError.', () async {
+      when(mockUserRepository.isUserExist())
+          .thenAnswer((_) async => Left(hiveException));
+
+      var result = await checkRegistrationTransaction.call(NoRequest());
+      verify(mockUserRepository.isUserExist());
+      expect(result, Left(hiveException));
       verifyNoMoreInteractions(mockUserRepository);
     });
   });
