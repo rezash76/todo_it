bb883106679912d08297f7c73ee8fc1adb963b74
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-19 13:36:19 +0330

    signout transaction tests completed.

--- test/features/authentication/domain/transaction/signout_transaction_test.dart
+++ test/features/authentication/domain/transaction/signout_transaction_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:todo_test/common/error/cache.dart';
 import 'package:todo_test/common/value_object/no_request.dart';
 import 'package:todo_test/common/value_object/void_operation.dart';
 import 'package:todo_test/features/athentication/domain/transaction/signout_transaction.dart';
@@ -10,10 +11,15 @@ import 'check_registration_transaction_test.mocks.dart';
 void main() {
   late SignoutTransaction signoutTransaction;
   late MockUserRepository mockUserRepository;
+  // exception
+  late TypeMissmatch typeMissmatch;
+  late HiveException hiveException;
 
   setUp(() {
     mockUserRepository = MockUserRepository();
     signoutTransaction = SignoutTransaction(repository: mockUserRepository);
+    typeMissmatch = TypeMissmatch();
+    hiveException = HiveException();
   });
 
   group('Signout transaction', () {
@@ -24,6 +30,26 @@ void main() {
       var result = await signoutTransaction.call(NoRequest());
       verify(mockUserRepository.signout());
       expect(result, Right(VoidOperation().voidValue));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return TypeMissmatch Exception.', () async {
+      when(mockUserRepository.signout())
+          .thenAnswer((_) async => Left(typeMissmatch));
+
+      var result = await signoutTransaction.call(NoRequest());
+      verify(mockUserRepository.signout());
+      expect(result, Left(typeMissmatch));
+      verifyNoMoreInteractions(mockUserRepository);
+    });
+
+    test('should return HiveException Exception.', () async {
+      when(mockUserRepository.signout())
+          .thenAnswer((_) async => Left(hiveException));
+
+      var result = await signoutTransaction.call(NoRequest());
+      verify(mockUserRepository.signout());
+      expect(result, Left(hiveException));
       verifyNoMoreInteractions(mockUserRepository);
     });
   });
