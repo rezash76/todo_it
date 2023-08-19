5703c23777a445244929d7471d91d4311c82f8a5
Reza Sharifi <reza.sharifi76@gmail.com>
2023-08-19 14:16:09 +0330

    update task transaction tests completed.

--- test/features/todo/domain/transaction/update_task_transaction_test.dart
+++ test/features/todo/domain/transaction/update_task_transaction_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:todo_test/common/error/cache.dart';
 import 'package:todo_test/features/todo/domain/entity/task_entity.dart';
 import 'package:todo_test/features/todo/domain/transaction/update_task_transaction.dart';
 import 'package:todo_test/features/todo/domain/value_object/task_request.dart';
@@ -10,20 +11,29 @@ import 'add_new_task_transaction_test.mocks.dart';
 
 void main() {
   late MockTaskRepository mockTaskRepository;
-  late UpdateTaskTransaction deleteTaskTransaction;
-  List<TaskEntity> tasks = [
-    TaskEntity('asq3d', 'asef', false),
-    TaskEntity('aedwsd', 'asef', true),
-    TaskEntity('aseesfd', 'asef', true),
-    TaskEntity('asegd', 'asef', false),
-  ];
-
-  final taskReq = TaskRequest('title', 'desc', true);
-  var request = UpdateTaskRequest(taskReq, 4);
+  late UpdateTaskTransaction updateTaskTransaction;
+  late List<TaskEntity> tasks;
+  late TaskRequest taskReq;
+  late UpdateTaskRequest request;
+  // exception
+  late NotFound notFound;
+  late TypeMissmatch typeMissmatch;
+  late HiveException hiveException;
 
   setUp(() {
     mockTaskRepository = MockTaskRepository();
-    deleteTaskTransaction = UpdateTaskTransaction(mockTaskRepository);
+    updateTaskTransaction = UpdateTaskTransaction(mockTaskRepository);
+    tasks = [
+      TaskEntity('asq3d', 'asef', false),
+      TaskEntity('aedwsd', 'asef', true),
+      TaskEntity('aseesfd', 'asef', true),
+      TaskEntity('asegd', 'asef', false),
+    ];
+    taskReq = TaskRequest('title', 'desc', true);
+    request = UpdateTaskRequest(taskReq, 4);
+    notFound = NotFound();
+    typeMissmatch = TypeMissmatch();
+    hiveException = HiveException();
   });
 
   group('Update tasks', () {
@@ -31,9 +41,39 @@ void main() {
       when(mockTaskRepository.updateTask(any))
           .thenAnswer((_) async => Right(tasks));
 
-      var result = await deleteTaskTransaction.call(request);
+      var result = await updateTaskTransaction.call(request);
       verify(mockTaskRepository.updateTask(request));
       expect(result, Right(tasks));
+      verifyNoMoreInteractions(mockTaskRepository);
+    });
+
+    test('should return NotFound Exception', () async {
+      when(mockTaskRepository.updateTask(any))
+          .thenAnswer((_) async => Left(notFound));
+
+      var result = await updateTaskTransaction.call(request);
+      verify(mockTaskRepository.updateTask(request));
+      expect(result, Left(notFound));
+      verifyNoMoreInteractions(mockTaskRepository);
+    });
+
+    test('should return TypeMissmatch Exception', () async {
+      when(mockTaskRepository.updateTask(any))
+          .thenAnswer((_) async => Left(typeMissmatch));
+
+      var result = await updateTaskTransaction.call(request);
+      verify(mockTaskRepository.updateTask(request));
+      expect(result, Left(typeMissmatch));
+      verifyNoMoreInteractions(mockTaskRepository);
+    });
+
+    test('should return HiveException', () async {
+      when(mockTaskRepository.updateTask(any))
+          .thenAnswer((_) async => Left(hiveException));
+
+      var result = await updateTaskTransaction.call(request);
+      verify(mockTaskRepository.updateTask(request));
+      expect(result, Left(hiveException));
       verifyNoMoreInteractions(mockTaskRepository);
     });
   });
